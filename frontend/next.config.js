e5595602aa372d9d69cb2841a24d17066d796f27
dalbeee <acapelles@gmail.com>
2022-01-24 01:21:03 +0900

    misc: add frontend secure header

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,40 @@
-const withBundleAnalyzer =
-  process.env.ANALYZE &&
-  require("@next/bundle-analyzer")({
-    enabled: !!process.env.ANALYZE,
-  });
+// const nextSafe = require("next-safe");
+const isDockerBuildTime = !!process.env.ANALYZE;
+const isDev = process.env.NODE_ENV !== "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = process.env.ANALYZE && withBundleAnalyzer({});
+const withBundleAnalyzer = isDockerBuildTime
+  ? require("@next/bundle-analyzer")({
+      enabled: true,
+    })
+  : () => {};
 
-module.exports = {
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'none';img-src 'self' data:;script-src-elem 'self';connect-src 'self';prefetch-src 'self';child-src 'none';font-src https://fonts.googleapis.com https://fonts.gstatic.com;style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+  },
+];
+
+const config = {
   webpackDevMiddleware: (config) => {
     config.watchOptions = {
       poll: 1000,
@@ -17,4 +45,15 @@ module.exports = {
   images: {
     domains: [process.env.NEXT_PUBLIC_CONFIG_IMAGE_HOST],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        // headers: nextSafe({ isDev  }),
+        headers: isProduction && securityHeaders,
+      },
+    ];
+  },
 };
+
+module.exports = withBundleAnalyzer({ ...config });

887c07dd2ecf919045b43d2880aaca39d4773cb3
dalbeee <acapelles@gmail.com>
2022-06-02 17:29:17 +0900

    modify: remove unused packages & add circular progress

--- frontend/src/common/pages/SuspensePage.tsx
+++ frontend/src/common/pages/SuspensePage.tsx
@@ -1,9 +1,16 @@
-import CircularProgress from "@mui/material/CircularProgress";
-
 const SuspensePage = () => {
   return (
     <div className="z-10 flex fixed left-0 top-0 h-screen w-full justify-center items-center">
-      <CircularProgress />
+      <div
+        className="inline-block w-24 h-24 
+            border-8 
+            border-t-amber-500 
+            border-r-blue-500 
+            border-b-rose-500 
+            border-l-green-500 
+            rounded-full 
+            animate-spin"
+      ></div>
     </div>
   );
 };

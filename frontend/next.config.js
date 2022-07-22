0cecdb1525ab5ce57b73429a6f03e32dc3c4c0e6
dalbeee <acapelles@gmail.com>
2022-07-22 16:49:06 +0900

    update: packages & next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,6 @@
-const withPlugins = require("next-compose-plugins");
 const withExportImages = require("next-export-optimize-images");
 
-const isDockerBuildTime = !!process.env.ANALYZE;
 const isProduction = process.env.NODE_ENV === "production";
-
-const withBundleAnalyzerLoader = () =>
-  isDockerBuildTime
-    ? require("@next/bundle-analyzer")({
-        enabled: true,
-      })
-    : {};
 
 const securityHeaders = [
   {
@@ -35,27 +26,15 @@ const securityHeaders = [
   },
 ];
 
+/** @type {import('next').NextConfig} */
 const config = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  images: {
-    domains: ["storage-service", "localhost"],
-  },
   async headers() {
     const applySecurity = {
       source: "/:path*",
-      // headers: nextSafe({ isDev  })
       headers: securityHeaders,
     };
     return isProduction ? [applySecurity] : [];
   },
 };
 
-module.exports = withPlugins([withBundleAnalyzerLoader(), withExportImages], {
-  ...config,
-});
+module.exports = withExportImages(config);
